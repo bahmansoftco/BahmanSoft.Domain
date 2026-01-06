9b32515a19ae10fde74714cbba2078e7cc5b107a
Gashti <gashti@aftabeshafa.ir>
2026-01-06 16:08:59 +0330

    init project

---
+++ Common/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BahmanSoft.Doamin.Common
+{
+    public class Entity : Entity<int>, IEntity
+    {
+    }
+
+    public class Entity<T>
+    {
+        public T Id { get; set; }
+    }
+}
---
+++ Common/IAuditedEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace BahmanSoft.Doamin.Common
+{
+    public interface IAuditedEntity<TCreatorId, TCreatorEntity>
+    {
+        DateTime CreationTime { get; set; }
+        TCreatorId CreatorId { get; set; }
+        TCreatorEntity Creator { get; set; }
+    }
+}
